badc894e59fd6540569bdbfc67f3a1eef0be6941
Сергей Чумовских <sergeyka-lalala@yandex.ru>
2022-03-02 18:56:35 +0300

    create new project

---
+++ ClearCodeGB/ClearCodeGB/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ClearCodeGB
+//
+//  Created by Сергей Чумовских  on 02.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
